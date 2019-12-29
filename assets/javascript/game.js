1e247d7bcdcfe2d5bf70b54b52e12f5adcc2c1ad
hallieraew <hallieweintraub@gmail.com>
2019-12-29 11:03:38 -0600

    instructions and background

---
+++ assets/javascript/game.js
@@ -0,0 +1,17 @@
+var wins = 0;
+var loss = 0;
+var currentScore = 0;
+var targetScore = [];
+var crystalNum = 0;
+// win count
+// loss count
+// computer number
+// user score
+// random number for crystals
+// random score generator
+// click event for crystal select
+// updates to html display id.text(var to pass in)
+//true false if game is in progress
+// crystals reset their value and the computer number reset at beginning of game
+//
+
