a34f5bf5fd5cfee61639b40a45282dcefc5a45f2
hallieraew <hallieweintraub@gmail.com>
2020-01-02 17:58:28 -0600

    working game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,61 +1,65 @@
 
-    var wins = "";
-    var loss = "";
-    var currentScore = 0;
-    var crystalNum = "";
-    var targetScore = Math.floor(Math.random() * 120) + 19;
-    var inProgress = false;
-    
-    var crystalArray = [];
-    
-    // function genArray() {
-    //     crystalArray.push = Math.floor(Math.random() * 12) + 1; 
-    //     console.log(crystalArray);
-    // };
-    
-    $(document).ready(function () {
-    
-        $(".crystal-image").each(function setCrystal() {
+var wins = "";
+var loss = "";
+var currentScore = 0;
+var crystalNum = "";
+var targetScore = Math.floor(Math.random() * 120) + 19;
+var inProgress = false;
+
+var crystalArray = [];
+
+// function genArray() {
+//     crystalArray.push = Math.floor(Math.random() * 12) + 1; 
+//     console.log(crystalArray);
+// };
+
+$(document).ready(function () {
+
+    $(".crystal-image").each(function setCrystal() {
         crystalArray = Math.floor(Math.random() * 12) + 1;
         $(this).attr("data-value", crystalArray);
         console.log(crystalArray);
 
         $("#target-score").text(targetScore);
+
     }
     );
-        $(".crystal-image").on("click", function() {
-    
-            var crystalVal = ($(this).attr("data-value"));
-            crystalVal = parseInt(crystalVal);
-            console.log(crystalVal);
+    function reset() {
+        currentScore = 0;
+        crystalNum = "";
+        targetScore = Math.floor(Math.random() * 120) + 19;
+        $("#target-score").text(targetScore);
+    }
+    $(".crystal-image").on("click", function () {
 
-            console.log(targetScore);
+        var crystalVal = ($(this).attr("data-value"));
+        crystalVal = parseInt(crystalVal);
+        console.log(crystalVal);
 
-            currentScore += crystalVal;
-            $("#current-score").text(currentScore);
-            $("#win-text").text("Wins: " + wins);
-            $("#loss-text").text("Losses: " + loss);
+        console.log(targetScore);
+
+        currentScore += crystalVal;
+        $("#current-score").text(currentScore);
+        $("#win-text").text("Wins: " + wins);
+        $("#loss-text").text("Losses: " + loss);
 
 
-            if (currentScore === targetScore) {
-                wins++;
-                reset();
-            }
-
-            else if (currentScore > targetScore) {
-                loss++;
-                reset();
-            }
-
-            else {
-
-            }
-        });
-
-        function reset() {
-            currentScore = 0;
-            crystalNum = "";
-            targetScore = "";
+        if (currentScore === targetScore) {
+            wins++;
+            $("#current-score").text("You Win!");
+            reset();
         }
 
+        else if (currentScore > targetScore) {
+            loss++;
+            $("#current-score").text("You Lose!");
+            reset();
+        }
+
+        else {
+            console.log("keep er rollin");
+        }
     });
+
+
+});
